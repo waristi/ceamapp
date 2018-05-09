37d661c50adec8b52728b73e19bc770b98da05d7
Bernardo Zuluaga <bernardo.az27@gmail.com>
2018-05-09 08:50:04 -0500

    Prueba generar

--- routes/datosEstacion.js
+++ routes/datosEstacion.js
@@ -18,7 +18,46 @@ router.get('/', middleware.ensureAuthenticated, function(req, res, next){
 //GUARDAR DatosEstacion
 router.post('/', middleware.ensureAuthenticated, function(req, res, next){
 
-	var cantidadRegistros = new DatosEstacion(req.body);
+	var cantidad = req.body.cantidad;
+
+	for(var i=0; i<cantidad; i++){
+		var data = new DatosEstacion({
+			fecha: new Date(),
+			temperatura: 0,
+			tempAlta: 0,
+			tempBaja: 0,
+			humedad: 0,
+			puntoRocio: 0,
+			velocidadViento: 0,
+			direccionViento: 0,
+			vientoCorriente: 0,
+			altaVelocidadViento: 0,
+			altaDireccionViento: 0,
+			vientoFrio: 0,
+			indiceCalor: 0,
+			thw: 0,
+			thsw: 0,
+			barometro: 0,
+			lluvia: 0,
+			tasalluvia: 0,
+			radSolar: 0,
+			energiaSolar: 0,
+			radSolarAlta: 0,
+			etIn: 0,
+			eiIndiceuv: 0,
+			diasGradoCalentamiento: 0,
+			diasGradoEnfriamiento: 0,
+			estacion: 0
+		})
+
+		entidad.save(function(err, item){
+			if(err){return next(err)}	
+		}) 
+	}
+
+	res.status(HttpStatus.CREATED).send({message: "Entidad creada correctamente"});
+
+	/*var cantidadRegistros = new DatosEstacion(req.body);
  	var dato = new DatosEstacion
  		({
  			 	temperatura : 72,
@@ -27,7 +66,7 @@ router.post('/', middleware.ensureAuthenticated, function(req, res, next){
     	dato.save(function(err, item){
 		if(err){return next(err)}	
 		res.status(HttpStatus.CREATED).send({message: "DatosEstacion creada correctamente", id: item._id});
-	}) 
+	})*/
 })
 
 //EDITAR Estacion
